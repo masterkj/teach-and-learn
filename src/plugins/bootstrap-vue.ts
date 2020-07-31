f52a652e11f14bf5dc0865ceb581671dd6210f01
omar khadamaljamy <khadamaljamyomar@gmail.com>
2020-07-31 17:12:42 +0300

    init with custom style

---
+++ src/plugins/bootstrap-vue.ts
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import { BootstrapVue } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section
+    class="cid-rRqrmgrYv3 mbr-fullscreen mbr-parallax-background"
+    id="header2-e"
+  >
+    <div
+      class="mbr-overlay"
+      style="opacity: 0.5; background-color: rgb(35, 35, 35);"
+    ></div>
+
+    <div class="container align-center">
+      <div class="row justify-content-md-center">
+        <div class="mbr-white col-md-10">
+          <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">
+            علّم وإنت عم تتعلّم
+          </h1>
+
+          <p class="mbr-text pb-3 mbr-fonts-style display-5">
+            &nbsp;هدفنا هو رفع السوية العلمية في المدارس الحكومية بتزويدها
+            بالأساتذة الأكفاء لتغطية النقص الحاصل في الكادر التدريسي ومساعدة
+            &nbsp;طلاب الجامعات في موادهم الجامعية من قبل زملائهم في السنوات
+            الأعلى بمقابل مادي بسيط وتفعيل نظام التعليم بالتبادل
+          </p>
+          <div class="mbr-section-btn">
+            <router-link
+              class="btn btn-md btn-secondary display-4"
+              to="/singup"
+              >التسجيل</router-link
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
