3b6d9cb049bdcc26841102480e8ae1bf0741e600
omar khadamaljamy <khadamaljamyomar@gmail.com>
2020-07-31 18:15:25 +0300

    remove custom stile

---
+++ src/plugins/parallax.ts
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import parallax from 'vue-parallaxy'
+
+Vue.component('parallax',parallax)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,19 @@
 <template>
-  <section
-    class="cid-rRqrmgrYv3 mbr-fullscreen mbr-parallax-background"
-    id="header2-e"
-  >
-    <div
-      class="mbr-overlay"
-      style="opacity: 0.5; background-color: rgb(35, 35, 35);"
-    ></div>
-
-    <div class="container align-center">
-      <div class="row justify-content-md-center">
-        <div class="mbr-white col-md-10">
-          <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">
-            علّم وإنت عم تتعلّم
-          </h1>
-
-          <p class="mbr-text pb-3 mbr-fonts-style display-5">
-            &nbsp;هدفنا هو رفع السوية العلمية في المدارس الحكومية بتزويدها
-            بالأساتذة الأكفاء لتغطية النقص الحاصل في الكادر التدريسي ومساعدة
-            &nbsp;طلاب الجامعات في موادهم الجامعية من قبل زملائهم في السنوات
-            الأعلى بمقابل مادي بسيط وتفعيل نظام التعليم بالتبادل
-          </p>
-          <div class="mbr-section-btn">
-            <router-link
-              class="btn btn-md btn-secondary display-4"
-              to="/singup"
-              >التسجيل</router-link
-            >
-          </div>
-        </div>
+    <parallax class="large-hero">
+      <img
+        src="/images/background4.jpg"
+        class="large-hero__image"
+        alt="university photo"
+      />
+      <div class="large-hero__text text-center">
+        <h1 class="text-lg mb-3"> علم و تعلم </h1>
+        <p class="text-md">
+           هدفنا هو رفع السوية العلمية في المدارس الحكومية بتزويدها بالأساتذة الأكفاء لتغطية النقص الحاصل في الكادر التدريسي ومساعدة  طلاب الجامعات في موادهم الجامعية من قبل زملائهم في السنوات الأعلى بمقابل مادي بسيط وتفعيل نظام التعليم بالتبادل
+        </p>
+        <button class="btn btn-primary btn-lg mt-2">تسجيل حساب</button> 
+       
       </div>
-    </div>
-  </section>
+    </parallax>
 </template>
 
 <script>
